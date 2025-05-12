c7eaf8a68d1a98b1847073455329821eda1c625e
Khafia Ayyub <85446654+KhafiaAyyub@users.noreply.github.com>
2025-05-12 23:17:33 +0530

    Update 0001-two-sum.java

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,3 +1,13 @@
+
+# Using Map
+//key value pair
+//1.put  O(n) 
+//2.map.containskey
+//3.search the difference of the target in an array
+    //Test1: 3 5 7 4      12
+    //Test2: 5 3 4 7 9    10
+
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         // 3 5 7 4
@@ -13,3 +23,4 @@ class Solution {
         return null;
     }
 }
+
