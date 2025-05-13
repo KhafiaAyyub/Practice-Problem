f66d3ee4c78ca0583331b4277a563d61dc491404
Khafia Ayyub <85446654+KhafiaAyyub@users.noreply.github.com>
2025-05-13 15:44:50 +0530

    BuyAndSell

--- Two Sum.java
+++ Two Sum.java
@@ -1,8 +1,5 @@
-
+//TWO SUM
 # Brute Force
-
-
-
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         
@@ -14,6 +11,27 @@ class Solution {
             }
         }
         return null;
+    }
+}
+
+//Best Time to Buy and Sell Stock
+
+//7,1,5,3,6,4
+//1,2,3,4,5,6 //6-1=5 
+//max-min //min - buy , max - sell
+class Solution {
+    public int maxProfit(int[] prices) {
+       int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+       for(int i=0;i<prices.length;i++){
+        if(prices[i] <minPrice){
+            minPrice = prices[i];
+        } else if(prices[i] - minPrice > maxProfit){
+            maxProfit = prices[i] - minPrice;
+        }
+       }
+       return maxProfit; 
+        
     }
 }
 
