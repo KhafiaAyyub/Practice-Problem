c2065269d08e5be16776ca91ff685436779a9dc1
Khafia Ayyub <85446654+KhafiaAyyub@users.noreply.github.com>
2025-05-13 16:03:19 +0530

    added sol2

--- Solutions.java
+++ Solutions.java
@@ -16,6 +16,7 @@ class Solution {
 
 //Best Time to Buy and Sell Stock
 
+//sol1:
 //7,1,5,3,6,4
 //1,2,3,4,5,6 //6-1=5 
 //max-min //min - buy , max - sell
@@ -35,3 +36,19 @@ class Solution {
     }
 }
 
+//sol2:
+//7,1,5,3,6,4
+//1,2,3,4,5,6 //6-1=5 
+//time and space complexity - O(n)
+class Solution {
+    public int maxProfit(int[] prices) {
+       int minSoFar = prices[0];
+        int maxProfit = 0;
+       for(int i=0;i<prices.length;i++){
+        minSoFar = Math.min(minSoFar,prices[i]);
+        int profit = prices[i] - minSoFar;
+        maxProfit = Math.max(maxProfit,profit);
+       }
+      return maxProfit;  
+    }
+}
