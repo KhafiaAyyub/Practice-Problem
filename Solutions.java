21724314cc173164ebbc04270f9e2f5e4868e062
Khafia Ayyub <85446654+KhafiaAyyub@users.noreply.github.com>
2025-05-19 22:20:45 +0530

    Update Solutions.java

--- Solutions.java
+++ Solutions.java
@@ -93,5 +93,16 @@ class Solution {
 
 
 //Running Sum of 1d Array
+class Solution {
+    public int[] runningSum(int[] nums) {
+    //TC - O(n)
+    //SC - O(1)        
 
+        for(int i=1;i<nums.length;i++){
+            nums[i]+=nums[i-1];
+
+        }
+        return nums;
+    }
+}
 
