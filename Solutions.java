fd8b09ec09ea5cbc34ab44247d388abed65d9811
Khafia Ayyub <85446654+KhafiaAyyub@users.noreply.github.com>
2025-05-19 21:46:31 +0530

    Update Solutions.java

--- Solutions.java
+++ Solutions.java
@@ -77,7 +77,19 @@ class Solution {
 
 
 //Best Time to Buy and Sell Stock II
-
+class Solution {
+    public int maxProfit(int[] prices) {
+        //TC = O(n) - one loop
+        //SP = O(1) - no extra space
+        int max_p = 0;
+        for(int i=0;i<prices.length-1;i++){
+            if(prices[i]<prices[i+1]){
+                max_p += prices[i+1]-prices[i];
+            }
+        }
+        return max_p;
+    }
+}
 
 
 //Running Sum of 1d Array
