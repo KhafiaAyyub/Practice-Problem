97ddf2ac9129540386f6e78ca65728077ba1f854
Khafia Ayyub <85446654+KhafiaAyyub@users.noreply.github.com>
2025-12-18 23:20:24 +0530

    Update Solutions.java

--- Solutions.java
+++ Solutions.java
@@ -3,6 +3,7 @@
 # Brute Force
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        
         for(int i=0;i<nums.length;i++){
             for(int j=i+1;j<nums.length;j++){
                     if(nums[i] + nums[j] == target){
