7203cc51c150ec932a24a6074a68baa4f43a9d62
Khafia Ayyub <85446654+KhafiaAyyub@users.noreply.github.com>
2025-12-22 23:43:25 +0530

    Fix formattibng issues in Solutions.java

--- Solutions.java
+++ Solutions.java
@@ -10,7 +10,8 @@ class Solution {
                     if(nums[i] + nums[j] == target){
                         return new int[] {i,j};
                     }
-            
+
+                
             }
         }
         return null;
