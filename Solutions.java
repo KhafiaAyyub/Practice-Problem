8b46eba72eeb4803db98e88efee2c1ffc7c61abf
Khafia Ayyub <85446654+KhafiaAyyub@users.noreply.github.com>
2026-02-21 15:42:26 +0530

    Fix formatting issues in Solutions.java

--- Solutions.java
+++ Solutions.java
@@ -12,7 +12,6 @@ class Solution {
                     {
                         return new int[] {i,j};
                     }   
-                
             }
         }  
         return null;
