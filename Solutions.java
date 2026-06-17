96e995efae3d068a75144609cba5e85b4a3f8d7f
Khafia Ayyub <85446654+KhafiaAyyub@users.noreply.github.com>
2026-06-17 23:48:00 +0530

    Update Solutions.java

--- Solutions.java
+++ Solutions.java
@@ -13,6 +13,7 @@ class Solution{
             for(int j=i+1;j<nums.length;j++){
                     if(nums[i] + nums[j] == target)
                     {
+                        
 
                         
    return new int[] {i,j};       
