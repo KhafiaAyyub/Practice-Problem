3cd63bb943af2357483554dffa85ebcbe9e7c017
Khafia Ayyub <85446654+KhafiaAyyub@users.noreply.github.com>
2026-07-06 20:51:49 +0530

    Update Solutions.java

--- Solutions.java
+++ Solutions.java
@@ -11,7 +11,8 @@ class Solution{
                     if(nums[i] + nums[j] == target)
                     {
              
-   return new int[] {i,j};       
+   return new int[] {i,j};  
+                        
                     }                    } 
             
             }
